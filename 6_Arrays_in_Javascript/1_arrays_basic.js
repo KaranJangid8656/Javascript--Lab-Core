c34883188a717be7575ce443c75ea08d93d7a5b7
Karan Suthar <karansuthar@gmail.com>
2025-12-20 20:05:12 +0530

    arrays in javascript

---
+++ 6_Arrays_in_Javascript/1_arrays_basic.js
@@ -0,0 +1,33 @@
+// In javascript , arrays can hold any type of data such as numbers ,strings , objects etc
+// An array in javascript is an object
+// An array holds many values under a single name
+
+const { kMaxLength } = require("buffer")
+
+// Creating an empty array
+let a = []
+console.log(a)
+
+// Creating an array and initializing it with values 
+let b = [10,20,30]
+console.log(b)
+
+// Basic operations in Javascript Arrays
+
+// Accessing an element from an array using array index
+let c = ["html" , "css" , "javascript"]
+console.log(c[2])
+
+// Acccesing the last element of array
+
+let d = [10,20,30]
+let last = d[d.length - 1]
+console.log(last)
+
+// Modifying array elements
+
+let m = ["a","b","c","d"]
+console.log(m)
+
+m[1] = "k"
+console.log(m)
---
+++ 6_Arrays_in_Javascript/2_array_methods.js
@@ -0,0 +1,31 @@
+// Adding element in an array at the end using push()
+
+let a =  ["HTML","CSS","JS"]
+a.push("Node.js");
+
+// Adding an element at beginning of the array using unshift()
+
+a.unshift("React");
+console.log(a)
+
+// Removing elements from array 
+// To remove elements from array we have many methods
+
+// Removing an element at beginning of the array using shift()
+// It removes and returns the first element
+let b = [ 1,2,3,4,5]
+b.shift();
+console.log(b)
+
+// Remove last element using pop()
+let c = [ 1,2,3,4,5]
+c.pop()
+console.log(c)
+
+
+
+
+
+
+
+
